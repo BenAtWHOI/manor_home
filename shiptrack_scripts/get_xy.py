bc33025959dab4fe319025f7a77aefa51df9fc06
Mario Carloni <mcarloni4@gmail.com>
2024-08-05 15:58:31 -0400

    downgrade get_xy to python 27

--- shiptrack_scripts/get_xy.py
+++ shiptrack_scripts/get_xy.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     out_file = 'gmt.xy'
     out_dir = '/home/data/shiptrack'
     data_dir = '/home/data/underway/proc'
-    last_out = f'{out_dir}/last.xy'
-    label = f'{out_dir}/gmt.label'
-    minmax = f'{out_dir}/minmax'
+    last_out = '{}/last.xy'.format(out_dir)
+    label = '{}/gmt.label'.format(out_dir)
+    minmax = '{}/minmax'.format(out_dir)
 
     #########################################
     # out_dir='/home/atlas/WHOI/ship_tracker'
@@ -27,25 +27,25 @@ if __name__ == '__main__':
     ship_id = cruise_id[:2]
 
     if debug:
-        print(f'out_file = {out_file}')
-        print(f'out_dir = {out_dir}')
-        print(f'data_dir = {data_dir}')
-        print(f'last_out = {last_out}')
-        print(f'label = {label}')
-        print(f'minmax = {minmax}')
-        print(f'cruise_id = {cruise_id}')
-        print(f'ship_id = {ship_id}')
+        print('out_file = {}'.format(out_file))
+        print('out_dir = {}'.format(out_dir))
+        print('data_dir = {}'.format(data_dir))
+        print('last_out = {}'.format(last_out))
+        print('label = {}'.format(label))
+        print('minmax = {}'.format(minmax))
+        print('cruise_id = {}'.format(cruise_id))
+        print('ship_id = {}'.format(ship_id))
 
     lats, lons = [], []
     
     # Find all CSVs in the data directory
     csv_file_list = []
     for filename in os.listdir(data_dir):
-        if re.search(rf"{ship_id}(\d{{6}})_.*\.csv", filename, flags=re.IGNORECASE):
+        if re.search(r'{}(\d{{6}})_.*\.csv'.format(ship_id), filename, flags=re.IGNORECASE):
             csv_file_list.append(os.path.join(data_dir, filename))
 
     if debug:
-        print(f'csv_file_list = {csv_file_list}')
+        print('csv_file_list = {}'.format(csv_file_list))
         
     try:
         # Open the output file once outside the loop
@@ -59,7 +59,7 @@ if __name__ == '__main__':
                         # Discard title line
                         skipping = next(f)
                         if debug:
-                            print(f'skipping title: {skipping}')
+                            print('skipping title: {}'.format(skipping))
 
                         # Read content
                         for row in reader:
@@ -73,14 +73,14 @@ if __name__ == '__main__':
                                     lat = float(lat_str)
                                     lon = float(lon_str)
                                     # Check for valid lat/lon
-                                    writer.write(f'{lon}, {lat}, {time}, {date}\n')
+                                    writer.write('{}, {}, {}, {}\n'.format(lon, lat, time, date))
                                     lats.append(lat)
                                     lons.append(lon)
                                 except ValueError:
                                     # Handle conversion error
-                                    print(f'Invalid lat/lon data: {lat_str}, {lon_str}')
+                                    print('Invalid lat/lon data: {}, {}'.format(lat_str, lon_str))
                 except FileNotFoundError:
-                    print(f'Unable to open file \'{csv_file}\'.')
+                    print('Unable to open file \'{}\'.'.format(csv_file))
 
         if lats and lons:
             # Write min and max values
@@ -92,8 +92,8 @@ if __name__ == '__main__':
             lol = lons[0]
             loh = lons[-1]
 
-            with open(f"{out_dir}/{minmax}", "w") as minmax_file:
-                minmax_file.write(f"{lal}, {lah}, {lol}, {loh}\n")
+            with open("{}/{}".format(out_dir, minmax), "w") as minmax_file:
+                minmax_file.write("{}, {}, {}, {}\n".format(lal, lah, lol, loh))
 
             # Current time as placeholders for now and now_t
             now = datetime.now().strftime('%Y-%m-%d')
@@ -101,11 +101,11 @@ if __name__ == '__main__':
 
             # Write last out values
             with open(last_out, "w") as last_out_file:
-                last_out_file.write(f"{lons[-1]} {lats[-1]} {now} {now_t} \n")
+                last_out_file.write("{} {} {} {} \n".format(lons[-1], lats[-1], now, now_t))
 
             # Write label values
             with open(label, "w") as label_file:
-                label_file.write(f"{lons[-1]} {lats[-1]} 16 0 6 1 {cruise_id}\n")
+                label_file.write("{} {} 16 0 6 1 {}\n".format(lons[-1], lats[-1], cruise_id))
 
                 if lats[-1] < 0:
                     latc = abs(lats[-1])
@@ -121,7 +121,7 @@ if __name__ == '__main__':
                     lonc = lons[-1]
                     EW = "E"
 
-                lastmsg = f"Last position: {latc} {NS}, {lonc} {EW} at {now_t} gmt on {now}\n"
-                label_file.write(f"{lons[-1]} {lats[-1]} 14 0 4 1 {lastmsg}\n")
+                lastmsg = "Last position: {} {}, {} {} at {} gmt on {}\n".format(latc, NS, lonc, EW, now_t, now)
+                label_file.write("{} {} 14 0 4 1 {}\n".format(lons[-1], lats[-1], lastmsg))
     except IOError as e:
-        print(f"Error opening output file: {e}")
+        print("Error opening output file: {}".format(e))
